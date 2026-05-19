fa0a8589e03f4a41a0a1abeef19a2969e35171f6
Exoluys <jairevankar98@gmail.com>
2026-05-20 04:44:28 +0530

    Validation Message fixed

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -29,7 +29,7 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(request=self.context.get('request'), email=attrs['email'], password=attrs['password'])
 
         if not user:
-            raise serializers.ValidationError("User Not Found")
+            raise serializers.ValidationError("Invalid email or password")
 
         attrs['user'] = user
         return attrs
